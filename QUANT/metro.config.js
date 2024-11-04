f936f0cb5344460ddd0e306bff1999ef25d7f76e
Jun279 <33.beautifulboy@gmail.com>
2024-11-05 00:34:03 +0900

    Update metro.config.js

--- QUANT/metro.config.js
+++ QUANT/metro.config.js
@@ -1,35 +1,14 @@
-// const { getDefaultConfig } = require("expo/metro-config");
-
-// module.exports = (() => {
-//   const config = getDefaultConfig(__dirname);
-
-//   const { transformer, resolver } = config;
-
-//   config.transformer = {
-//     ...transformer,
-//     babelTransformerPath: require.resolve("react-native-svg-transformer"),
-//   };
-//   config.resolver = {
-//     ...resolver,
-//     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-//     sourceExts: [...resolver.sourceExts, "svg"],
-//   };
-
-//   return config;
-// })();
-
 const { getDefaultConfig } = require("expo/metro-config");
-const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
-module.exports = wrapWithReanimatedMetroConfig(() => {
+module.exports = (() => {
   const config = getDefaultConfig(__dirname);
+
   const { transformer, resolver } = config;
 
   config.transformer = {
     ...transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
   };
-
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
